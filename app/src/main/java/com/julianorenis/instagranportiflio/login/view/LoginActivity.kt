498250a9c3e786700ad58745aad1cf2cdd18582f
JULIANO RENIS GOMES SILVA DOS SANTOS <julianorenis@gmail.com>
2023-01-02 22:58:03 -0300

    Instagran Portifólio - Construindo tela de login
    - Componentização layout
    - Criando style unicos  para TextViews, EditText, TextView e Button
    - Criando style container pára EditText
    - Usando formatação no xml: Deixar negrito uma parte da frase semm precisar dividir a string
    - Usamos Framelayout para criar a linha de "ou"
    - Afirmando conceitos de constraints: verticalBias"0.3" para mudar em pocentagem a altura da linearlayout que esta dentro da constraint
    
    - Criando arquivo selctor para apresentar a mudança de cor no clique do botão.

---
+++ app/src/main/java/com/julianorenis/instagranportiflio/login/view/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.julianorenis.instagranportiflio.login.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.julianorenis.instagranportiflio.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
