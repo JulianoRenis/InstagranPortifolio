ab4fc6e6ba96fb5c3c6a074171e4994d0dda4840
JULIANO RENIS GOMES SILVA DOS SANTOS <julianorenis@gmail.com>
2023-01-03 21:24:55 -0300

    Validando formulários
    -Criando envelopamento dos editTexts para criar mensagens de erros: TextInputLayout
    -Criar estilos para os envelopamentos pois temos que envelopar os editTexts separadamente.

--- app/src/main/java/com/julianorenis/instagranportiflio/login/view/LoginActivity.kt
+++ app/src/main/java/com/julianorenis/instagranportiflio/login/view/LoginActivity.kt
@@ -2,11 +2,16 @@ package com.julianorenis.instagranportiflio.login.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
+import com.google.android.material.textfield.TextInputLayout
 import com.julianorenis.instagranportiflio.R
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        findViewById<TextInputLayout>(R.id.login_edit_email_input)
+            .error = "Esse e-mail é invalido"
     }
 }
