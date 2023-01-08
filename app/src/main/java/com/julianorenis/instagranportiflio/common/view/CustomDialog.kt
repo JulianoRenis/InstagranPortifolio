53de96de43635e99f608ef02a8c0573d1e32ce55
JULIANO RENIS GOMES SILVA DOS SANTOS <julianorenis@gmail.com>
2023-01-08 17:05:25 -0300

    Criando Dialog persnolisado

--- app/src/main/java/com/julianorenis/instagranportiflio/common/view/CustomDialog.kt
+++ app/src/main/java/com/julianorenis/instagranportiflio/common/view/CustomDialog.kt
@@ -2,6 +2,58 @@ package com.julianorenis.instagranportiflio.common.view
 
 import android.app.Dialog
 import android.content.Context
+import android.os.Bundle
+import android.view.View
+import android.widget.LinearLayout
+import android.widget.TextView
+import com.julianorenis.instagranportiflio.R
 
-class CustomDialog(context: Context): Dialog(context) {
+class CustomDialog(context: Context) : Dialog(context) {
+
+    private lateinit var dialogLinearlayout: LinearLayout
+    private lateinit var txtButtons: Array<TextView>
+
+    private var titleId: Int? = null
+    private lateinit var textView: TextView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.dialog_custom)
+
+        dialogLinearlayout = findViewById(R.id.dialog_container)
+        textView = findViewById(R.id.dialog_title)
+    }
+
+    override fun setTitle(title: CharSequence?) {
+        super.setTitle(title)
+    }
+
+    fun addButtom( vararg texts: Int,listener: View.OnClickListener) {
+        txtButtons = Array(texts.size) {
+            TextView(context)
+        }
+
+        texts.forEachIndexed{index, txtId ->
+            txtButtons[index].id = txtId
+            txtButtons[index].setText(txtId)
+            txtButtons[index].setOnClickListener {
+                listener.onClick(it)
+                dismiss()
+            }
+        }
+    }
+
+    override fun show() {
+        super.show()
+
+        titleId?.let {
+            textView.setText(it)
+        }
+        for (textView in txtButtons){
+
+            val layoutParams = LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT,LinearLayout.LayoutParams.WRAP_CONTENT)
+            layoutParams.setMargins(30,50,30,50)
+            dialogLinearlayout.addView(textView,layoutParams)
+        }
+    }
 }
--- app/src/main/java/com/julianorenis/instagranportiflio/view/RegisterPhotoFragment.kt
+++ app/src/main/java/com/julianorenis/instagranportiflio/view/RegisterPhotoFragment.kt
@@ -1,11 +1,14 @@
 package com.julianorenis.instagranportiflio.view
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import com.julianorenis.instagranportiflio.R
+import com.julianorenis.instagranportiflio.common.view.CustomDialog
 
 class RegisterPhotoFragment : Fragment() {
 
@@ -15,5 +18,26 @@ class RegisterPhotoFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_register_photo,container,false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val customDialog = CustomDialog(requireContext())
+        customDialog.addButtom(R.string.photo, R.string.gallery){
+            when(it.id){
+                R.string.photo -> {
+                    Log.i("Teste","Foto")
+                }
+                R.string.gallery ->{
+                    Log.i("Teste","Galeria")
+                }
+
+            }
+        }
+//        customDialog.addButtom({
+//        Log.i("Teste",(it as TextView).text.toString())
+//        },R.string.photo,R.string.gallery)
+        customDialog.show()
     }
 }
